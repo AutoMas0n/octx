3d223cf139679f4309d8d70f064c3b7ac78cfcdf
AutoMas0n <jesse1819@gmail.com>
2026-08-20 17:58:14 -0400

    chore: remove examples dir

--- examples/paths_demo.rs
+++
@@ -1,8 +0,0 @@
-use octx::paths;
-
-fn main() {
-    println!("📁  Data  dir: {}", paths::data_dir().display());
-    println!("📁  Config dir: {}", paths::config_dir().display());
-    println!("📁  Bin    dir: {}", paths::bin_dir().display());
-    println!("📁  Skills dir: {}", paths::skills_dir().display());
-}
